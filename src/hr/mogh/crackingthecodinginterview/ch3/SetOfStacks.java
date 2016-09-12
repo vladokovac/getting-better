97c0346f4c3f1f70b9a5e0f888af208294b2ab55
vlado <vladimir.kovac2811@gmail.com>
2016-09-12 21:10:21 +0200

    #43: Set of Stacks. Added documentation.

--- src/hr/mogh/crackingthecodinginterview/ch3/SetOfStacks.java
+++ src/hr/mogh/crackingthecodinginterview/ch3/SetOfStacks.java
@@ -4,6 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Solve the following problem:
+ * <p/>
+ * <code>Imagine a (literal) stack of plates. If the stack gets too high, it might topple. Therefore, in real life,
+ * we would likely start a new stack when the previous stack exceeds some threshold. Implement a data structure
+ * SetOfStacks that mimics this. SetOfStacks should be composed of several stacks and should create a new stack once
+ * the previous one exceeds capacity. SetOfStacks.push() and SetOfStacks.pop() should behave identically to a single
+ * stack (that is, pop() should return the same values as it would if there were just a single stack).</code>
+ * <p/>
  * Created by vlado on 12.9.2016.
  */
 public class SetOfStacks {
@@ -21,6 +29,12 @@ public class SetOfStacks {
         topStackCount = 0;
     }
 
+    /**
+     * A push operation.<br/>
+     * Time complexity: <code>O(1)</code>.
+     *
+     * @param value The new value.
+     */
     public void push(Object value) {
         if (topStackCount == singleStackSize) {
             // add a new top stack
@@ -34,6 +48,12 @@ public class SetOfStacks {
         topStack[topStackCount++] = value;
     }
 
+    /**
+     * The pop operation.<br/>
+     * Time complexity:<code>O(1)</code>.
+     *
+     * @return The popped value.
+     */
     public Object pop() {
         if (topStackCount == 0) {
             // remove the top stack (it's empty)
